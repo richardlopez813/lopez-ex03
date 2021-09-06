98489ff254b6db6f9850096c82e3b64288ad8a8a
richard lopez <richardlopez813>
2021-09-05 22:07:25 -0400

    final

--- src/main/java/exercise03/Solution03.java
+++ src/main/java/exercise03/Solution03.java
@@ -1,3 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Richard Lopez
+ */
+
+/*
+    1. Input Scanner
+    2. prompt user for their quote of choice
+    3. Scan for quote
+    4. prompt user for the quote's author
+    5. Scan for author name
+    6. Output result with inputs
+ */
+
 package exercise03;
 
 import java.util.Scanner;
